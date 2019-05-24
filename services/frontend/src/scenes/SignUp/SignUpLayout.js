e40f19e43b6de5b75b4a1e1c9735dca4da63c3a1
Devin Sit <devin@devinsit.com>
2019-05-24 10:41:54 -0400

    CDHSH-50 Created the base layout for the SignUp scene.

--- services/frontend/src/scenes/SignUp/SignUpLayout.js
+++ services/frontend/src/scenes/SignUp/SignUpLayout.js
@@ -1,9 +1,41 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import {Logo} from "assets/icons";
 import "./SignUp.scss";
 
 const SignUpLayout = () => (
-    <div className="sign-up">
-        Sign Up
+    <div className="signup">
+        <SignUpForm />
+        <SignUpBranding />
+    </div>
+);
+
+const SignUpForm = () => (
+    <div className="signup-form">
+        <div className="signup-form-card">
+            <h3 className="signup-form-title">SIGN UP</h3>
+
+            <TextField
+                className="signup-form-email"
+                label="Email"
+            />
+
+            <TextField
+                className="signup-form-password"
+                label="Password"
+            />
+
+            <button className="signup-form-submit">SIGN UP</button>
+        </div>
+    </div>
+);
+
+const SignUpBranding = () => (
+    <div className="signup-branding">
+        <div className="signup-branding-container">
+            <Logo className="signup-branding-logo" />
+            <h1 className="signup-branding-name">SKILLHUB</h1>
+        </div>
     </div>
 );
 
