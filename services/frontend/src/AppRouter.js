179f9de99fe0109858c582d088506dc21e9acaec
Liannus <josh-gorman@hotmail.com>
2019-05-28 12:54:07 -0400

    CDHSH-14 Style fixes to fall in line with code-review

--- services/frontend/src/AppRouter.js
+++ services/frontend/src/AppRouter.js
@@ -2,17 +2,17 @@ import React from "react";
 import {Switch, Route} from "react-router";
 import {Profile, People} from "scenes/";
 import ScreenUrls from "utils/screenUrls";
-import { Navbar } from "components/";
+import {Navbar} from "components/";
 
 const AppRouter = () => (
-    <div id="app">
+    <>
         <Navbar />
         <Switch>
             <Route path={ScreenUrls.PROFILE} component={Profile} />
             <Route path={ScreenUrls.PEOPLE} component={People} />
-            <Route path={ScreenUrls.PROFILE} component={Profile} />
+            <Route path={ScreenUrls.PROJECTS} component={Profile} />
         </Switch>
-    </div>
+    </>
 );
 
 export default AppRouter;
--- services/frontend/src/components/Navbar/NavbarLayout.js
+++ services/frontend/src/components/Navbar/NavbarLayout.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { Logo } from "assets/icons";
-import { Button, Tabs, Tab } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import {Logo} from "assets/icons";
+import {Button, Tabs, Tab} from "@material-ui/core";
+import {Link} from "react-router-dom";
 import ScreenUrls from "utils/screenUrls";
 import "./Navbar.scss";
 
 const NavbarLayout = () => {
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState(0);
 
-    function handleChange(event, newValue) {
+    const handleChange = (event, newValue) => {
         setValue(newValue);
     }
 
     return (
         <div className="navbar">
-            <a className="navbar-brand" href="/">
+            <Link to={ScreenUrls.LANDING} className="navbar-brand">
                 <Logo className="logo-icon" />
-            </a>
+            </Link>
+
             <Tabs className="navbar-links" value={value} onChange={handleChange}>
                 <Tab to={ScreenUrls.PROFILE} component={Link} label="My Profile" />
                 <Tab to={ScreenUrls.PEOPLE} component={Link} label="People" />
-                <Tab to={ScreenUrls.PEOPLE} component={Link} label="Projects" />
+                <Tab to={ScreenUrls.PROJECTS} component={Link} label="Projects" />
             </Tabs>
 
             <div className="navbar-controls">
