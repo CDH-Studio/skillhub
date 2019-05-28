64d6ef7fb969fb68afcf89def774c920ca0ae2ab
Devin Sit <devin@devinsit.com>
2019-05-28 11:28:45 -0400

    CDHSH-51 Added error handling, validation, loading states, and 'on enter' input handlers to the SignUp form.

--- services/frontend/src/scenes/SignUp/SignUp.js
+++ services/frontend/src/scenes/SignUp/SignUp.js
@@ -1,25 +1,81 @@
-import React, {useCallback} from "react";
-import {useInput} from "utils/hooks";
+import React, {useCallback, useState} from "react";
+import {useInput, useOnEnterKey} from "utils/hooks";
 import api from "api/";
 import SignUpLayout from "./SignUpLayout";
 
+const EMAIL_REGEX = /^\S+@\S+$/;
+
+const errorMessageMap = {
+    "Validation error": "This user already exists",
+    "Missing credentials": "Form cannot be empty"
+};
+
+const validateUserInfo = (email, password) => {
+    if (!email || !password) {
+        throw new Error("Missing credentials");
+    }
+
+    if (!EMAIL_REGEX.test(email)) {
+        throw new Error("Invalid email");
+    }
+};
+
+const onSignUp = async (email, password, setErrorMessage, setInvalidEmail, setIsLoading) => {
+    const clearInfoState = () => {
+        setIsLoading(false);
+        setErrorMessage("");
+        setInvalidEmail(false);
+    };
+
+    try {
+        validateUserInfo(email, password);
+        setIsLoading(true);
+
+        await api.service("users").create({email, password});
+
+        clearInfoState();
+    } catch (error) {
+        const {message} = error;
+
+        clearInfoState();
+
+        if (message in errorMessageMap) {
+            setErrorMessage(errorMessageMap[message]);
+        } else if (message === "Invalid email") {
+            setInvalidEmail(true);
+        } else {
+            setErrorMessage(message);
+        }
+    }
+};
+
 const SignUp = () => {
+    const [errorMessage, setErrorMessage] = useState("");
+    const [invalidEmail, setInvalidEmail] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
     const emailInput = useInput();
     const passwordInput = useInput();
 
-    const onSignUpClick = useCallback(async () => {
-        try {
-            await api.service("users").create({email: emailInput.value, password: passwordInput.value});
-        } catch (e) {
-            console.log(e);
-        }
-    }, [emailInput, passwordInput]);
+    const {value: email} = emailInput;
+    const {value: password} = passwordInput;
+
+    const onSignUpClick = useCallback(
+        async () => onSignUp(email, password, setErrorMessage, setInvalidEmail, setIsLoading),
+        [email, password, setErrorMessage, setInvalidEmail, setIsLoading]
+    );
+
+    const onInputEnter = useCallback(useOnEnterKey(onSignUpClick), [onSignUpClick]);
 
     return (
         <SignUpLayout
             emailInput={emailInput}
             passwordInput={passwordInput}
+            isLoading={isLoading}
+            errorMessage={errorMessage}
+            invalidEmail={invalidEmail}
             onSignUpClick={onSignUpClick}
+            onInputEnter={onInputEnter}
         />
     );
 };
--- services/frontend/src/scenes/SignUp/SignUpLayout.js
+++ services/frontend/src/scenes/SignUp/SignUpLayout.js
@@ -1,34 +1,34 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import TextField from "@material-ui/core/TextField";
+import classNames from "classnames";
+import {Button, Card, CardContent, CircularProgress, TextField} from "@material-ui/core";
+import {Error} from "@material-ui/icons";
 import {Link} from "react-router-dom";
 import {Logo} from "assets/icons";
 import ScreenUrls from "utils/screenUrls";
 import "./SignUp.scss";
 
-const SignUpLayout = ({emailInput, passwordInput, onSignUpClick}) => (
+const SignUpLayout = (formProps) => (
     <div className="signup">
-        <SignUpForm
-            emailInput={emailInput}
-            passwordInput={passwordInput}
-            onSignUpClick={onSignUpClick}
-        />
+        <SignUpForm {...formProps} />
 
         <SignUpBranding />
     </div>
 );
 
-const SignUpForm = ({emailInput, passwordInput, onSignUpClick}) => (
+const SignUpForm = ({
+    emailInput, passwordInput, isLoading, errorMessage, invalidEmail, onSignUpClick, onInputEnter
+}) => (
     <div className="signup-form">
         <Card>
             <CardContent className="signup-form-card-content">
                 <h3 className="signup-form-title">SIGN UP</h3>
 
-                <TextField
+                <FormError message={errorMessage} />
+
+                <EmailInput
                     className="signup-form-email"
-                    label="Email"
+                    invalid={invalidEmail}
+                    onKeyPress={onInputEnter}
                     {...emailInput}
                 />
 
@@ -36,17 +36,19 @@ const SignUpForm = ({emailInput, passwordInput, onSignUpClick}) => (
                     className="signup-form-password"
                     label="Password"
                     type="password"
+                    onKeyPress={onInputEnter}
                     {...passwordInput}
                 />
 
-                <Button
+                <LoadingButton
                     className="signup-form-submit"
                     color="primary"
                     variant="contained"
+                    loading={isLoading}
                     onClick={onSignUpClick}
                 >
                     SIGN UP
-                </Button>
+                </LoadingButton>
             </CardContent>
         </Card>
     </div>
@@ -61,6 +63,44 @@ const SignUpBranding = () => (
             </div>
         </Link>
     </div>
+);
+
+const EmailInput = ({className, invalid = false, ...otherProps}) => (
+    <TextField
+        className={classNames("email-input", className)}
+        label="Email"
+        error={invalid}
+        helperText={invalid ? "Invalid email" : ""}
+        {...otherProps}
+    />
+);
+
+const FormError = ({className, message = ""}) => (
+    <div
+        className={classNames(
+            "form-error",
+            {"form-error--visible": message !== ""},
+            className
+        )}
+    >
+        <Error color="error" fontSize="small" />
+        <p className="form-error-message">{message}</p>
+    </div>
+);
+
+const LoadingButton = ({className, loading, children, ...otherProps}) => (
+    <Button
+        className={classNames("loading-button", className)}
+        {...otherProps}
+    >
+        {
+            loading ? (
+                <CircularProgress className="loading-button-indicator" />
+            ) : (
+                children
+            )
+        }
+    </Button>
 );
 
 export default SignUpLayout;
