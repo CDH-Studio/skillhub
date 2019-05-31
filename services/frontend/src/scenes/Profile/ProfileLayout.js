9586ce149531315164905b835dd50ee261ba24cb
Liannus <josh-gorman@hotmail.com>
2019-05-31 13:41:01 -0400

    CDHSH-60 Added container resizing to main profile page and added view for personal details

--- services/frontend/src/scenes/Profile/ProfileLayout.js
+++ services/frontend/src/scenes/Profile/ProfileLayout.js
@@ -1,77 +1,78 @@
 import React from "react";
 import Scrollspy from "react-scrollspy";
-import Grid from "@material-ui/core/Grid";
+import {Avatar} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
+import Email from "@material-ui/icons/Email";
+import LocalPhone from "@material-ui/icons/LocalPhone";
+import Icon from '@material-ui/core/Icon';
 import "./Profile.scss";
 
 const ProfileLayout = () => {
     return (
-        <Grid className="profile">
-            <ProfileSidebar />
-            <ProfileContent />
-        </Grid>
+        <div className="scrollable-container">
+            <div className="profile">
+                <ProfileSidebar />
+                <ProfileContent />
+            </div> 
+        </div>
     );
 };
 
 const ProfileSidebar = () => (
-    <Grid className="profile-sidebar" item={true} xs={4}>
+    <div className="profile-sidebar" item={true} xs={4}>
         <Scrollspy
             className="profile-scroll-nav"
             rootEl=".profile-content"
-            items={["personal-details", "skills", "projects"]}
+            items={["profile-personal-details", "profile-skills", "profile-projects"]}
             currentClassName="is-current"
         >
             <li className="profile-list-item">
-                <a href="#personal-details">Personal Details</a>
+                <a href="#profile-personal-details">Personal Details</a>
             </li>
             <li className="profile-list-item">
-                <a href="#skills">Skills</a>
+                <a href="#profile-skills">Skills</a>
             </li>
             <li className="profile-list-item">
-                <a href="#projects">Projects</a>
+                <a href="#profile-projects">Projects</a>
             </li>
         </Scrollspy>
-    </Grid>
+    </div>
 );
 
 const ProfileContent = () => (
-    <Grid className="profile-content" item={true} xs={8}>
-        <section id="personal-details">
-            <Paper className="profile-content-card">
-                Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
-                sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
-                rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
-                eur adipisicing ea irure. Elit voluptate cupidatat anim sit aute non excepteur Lorem nostrud occae
-                cat irure ut esse fugiat. Veniam proident esse aliqua do mollit laboris dolor. Adipisicing est nis
-                i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
+    <div className="profile-content" item={true} xs={8}>
+        <section id="profile-personal-details">
+            <Paper className="profile-card">
+                <Avatar className="profile-picture">
+                    DS
+                </Avatar> 
 
-                Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
-                sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
-                rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
-                eur adipisicing ea irure. Elit voluptate cupidatat anim sit aute non excepteur Lorem nostrud occae
-                cat irure ut esse fugiat. Veniam proident esse aliqua do mollit laboris dolor. Adipisicing est nis
-                i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
+                <div className="profile-card-content">
+                    <h2 className="profile-card-title">
+                        Devin Sit
+                    </h2>
 
-                <br /><br />
+                    <h3 className="profile-card-subtitle">
+                        Software Developer
+                    </h3>
 
-                Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
-                sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
-                rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
-                eur adipisicing ea irure. Elit voluptate cupidatat anim sit aute non excepteur Lorem nostrud occae
-                cat irure ut esse fugiat. Veniam proident esse aliqua do mollit laboris dolor. Adipisicing est nis
-                i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
+                    <div className="profile-card-contact">
+                        <p>
+                            <Email />
+                            Devin.Sit@Canada.ca
+                        </p>
 
-                Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
-                sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
-                rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
-                eur adipisicing ea irure. Elit voluptate cupidatat anim sit aute non excepteur Lorem nostrud occae
-                cat irure ut esse fugiat. Veniam proident esse aliqua do mollit laboris dolor. Adipisicing est nis
-                i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
+                        <p>
+                            <LocalPhone />
+                            123-456-7890
+                        </p>
+                    </div>
+                </div>
             </Paper>
         </section>
-        <section id="skills">
+        <section id="profile-skills">
             <h2>Skills</h2>
-            <Paper className="profile-content-card">
+            <Paper className="profile-card">
                 Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
                 sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
                 rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
@@ -103,9 +104,9 @@ const ProfileContent = () => (
                 i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
             </Paper>
         </section>
-        <section id="projects">
+        <section id="profile-projects">
             <h2>Projects</h2>
-            <Paper className="profile-content-card">
+            <Paper className="profile-card">
                 Occaecat reprehenderit fugiat qui ullamco ad commodo Lorem velit nisi aliquip sit esse officia con
                 sequat. Officia aliqua ut reprehenderit ex occaecat ut aute dolor amet deserunt veniam. Reprehende
                 rit Lorem laboris est consequat. Enim ipsum ea do esse non esse incididunt id deserunt elit except
@@ -137,7 +138,7 @@ const ProfileContent = () => (
                 i id nisi nisi amet anim nostrud eiusmod ad fugiat qui.
             </Paper>
         </section>
-    </Grid>
+    </div>
 );
 
 export default ProfileLayout;
