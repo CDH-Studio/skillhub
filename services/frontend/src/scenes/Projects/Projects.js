120354ce6b00f8b75c91287dd45451d7d5902080
Devin Sit <devin@devinsit.com>
2019-06-03 15:35:02 -0400

    CDHSH-69 Made the Project list filter buttons clickable.

--- services/frontend/src/scenes/Projects/Projects.js
+++ services/frontend/src/scenes/Projects/Projects.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ProjectsLayout from "./ProjectsLayout";
 
-const Projects = () => (
-    <ProjectsLayout />
-);
+class Projects extends React.Component {
+    state = {
+        activeFilter: "all"  // "all", "recentlyActive", "inactive"
+    };
+
+    onFilterClick = (filter) => () => {
+        this.setState({activeFilter: filter});
+    };
+
+    render() {
+        const {activeFilter} = this.state;
+
+        return (
+            <ProjectsLayout
+                activeFilter={activeFilter}
+                onFilterClick={this.onFilterClick}
+            />
+        );
+    }
+}
 
 export default Projects;
--- services/frontend/src/scenes/Projects/ProjectsLayout.js
+++ services/frontend/src/scenes/Projects/ProjectsLayout.js
@@ -2,32 +2,49 @@ import React from "react";
 import classNames from "classnames";
 import "./Projects.scss";
 
-const ProjectsLayout = () => (
-    <div>
-        <ProjectsFilters />
+const ProjectsLayout = ({activeFilter, onFilterClick}) => (
+    <div className="projects-list">
+        <ProjectsHeader
+            activeFilter={activeFilter}
+            onFilterClick={onFilterClick}
+        />
     </div>
 );
 
-const ProjectsFilters = () => (
-    <div className="projects-filters">
-        <FilterButton label="All" />
-        <FilterButton label="Recently Active" />
-        <FilterButton label="Inactive" />
+const ProjectsHeader = ({activeFilter, onFilterClick}) => (
+    <div className="projects-header">
+        <FilterButton
+            label="All"
+            isActive={activeFilter === "all"}
+            onClick={onFilterClick("all")}
+        />
+
+        <FilterButton
+            label="Recently Active"
+            isActive={activeFilter === "recentlyActive"}
+            onClick={onFilterClick("recentlyActive")}
+        />
+
+        <FilterButton
+            label="Inactive"
+            isActive={activeFilter === "inactive"}
+            onClick={onFilterClick("inactive")}
+        />
     </div>
 );
 
-const FilterButton = ({label, isActive = false}) => (
-    <button 
+const FilterButton = ({label, isActive = false, onClick}) => (
+    <button
         className={classNames(
-            "filter-button", 
+            "filter-button",
             {"filter-button--active": isActive}
         )}
-        variant="contained" 
+        variant="contained"
         color="primary"
+        onClick={onClick}
     >
         {label}
     </button>
 );
-
 
 export default ProjectsLayout;
