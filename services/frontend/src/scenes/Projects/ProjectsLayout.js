24855e18f97f36a8a9531a64ce5361b215780bf6
Devin Sit <devin@devinsit.com>
2019-06-03 15:09:53 -0400

    CDHSH-69 Setup the basic layout for the Projects filter header.

--- services/frontend/src/scenes/Projects/ProjectsLayout.js
+++ services/frontend/src/scenes/Projects/ProjectsLayout.js
@@ -1,10 +1,33 @@
 import React from "react";
+import classNames from "classnames";
 import "./Projects.scss";
 
 const ProjectsLayout = () => (
     <div>
-        Projects
+        <ProjectsFilters />
     </div>
 );
+
+const ProjectsFilters = () => (
+    <div className="projects-filters">
+        <FilterButton label="All" />
+        <FilterButton label="Recently Active" />
+        <FilterButton label="Inactive" />
+    </div>
+);
+
+const FilterButton = ({label, isActive = false}) => (
+    <button 
+        className={classNames(
+            "filter-button", 
+            {"filter-button--active": isActive}
+        )}
+        variant="contained" 
+        color="primary"
+    >
+        {label}
+    </button>
+);
+
 
 export default ProjectsLayout;
