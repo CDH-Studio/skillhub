f33009c1a02133052103fac1b1e594d0cf4b33e7
Josh <19962651+Liannus@users.noreply.github.com>
2019-06-05 13:38:57 -0400

    CDHSH-70 Added seed file for profile (#12)
    Changelog:
    - Added new seed file to the backend which holds dummy data for the profile.
    
    Other Implementation Details:
    - Added the authentication hook to the profile table to ensure that the user is authenticated before accessing data.
    - Generated a seedfile using sequalize and added dummy data to the file seedData.js

---
+++ services/backend/src/db/seeders/20190603191615-create-default-profile.js
@@ -0,0 +1,23 @@
+const tableNames = require("db/tableNames");
+const {PROFILES, PROFILE_IDS} = require("db/seedData");
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        const profiles = await queryInterface.rawSelect(tableNames.PROFILES, {
+            where: {
+                id: {
+                    [Sequelize.Op.in]: PROFILE_IDS
+                }
+            }
+        }, ["id"]);
+
+        if (!profiles) {
+            await queryInterface.bulkInsert(tableNames.PROFILES, PROFILES);
+        }
+    },
+    down: async (queryInterface, Sequelize) => {
+        const OpIn = Sequelize.Op.in;
+
+        await queryInterface.bulkDelete(tableNames.PROFILES, {id: {[OpIn]: PROFILE_IDS}});
+    }
+};
