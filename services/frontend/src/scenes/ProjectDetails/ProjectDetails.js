a8278266e252edcc45a6170f63e06d95dca50e23
bmalghan <bmalg082@uottawa.ca>
2019-06-05 10:27:39 -0400

    Revert "Revert "CDHSH-63""
    This reverts commit 0c1b1368

---
+++ services/frontend/src/scenes/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ProjectDetailsLayout from "./ProjectDetailsLayout";
+
+const ProjectDetails = () => (
+  <ProjectDetailsLayout />
+);
+
+export default ProjectDetails;
