56ab20133f54e060678ee6a393a46ec2ddb4ad5f
Devin Sit <devin@devinsit.com>
2019-06-10 13:36:59 -0400

    CDHSH-77 Created the layout for the Projects section of the Profile scene. (#21)
    Changelog:
    
    - Users can now see dummy projects on their personal profile page.
    
    Other Implementation Details:
    
    - Refactored the ProjectCard (and related components) into its own file.

--- services/frontend/src/components/index.js
+++ services/frontend/src/components/index.js
@@ -5,4 +5,5 @@ export {default as FormError} from "./FormError";
 export {default as LoadingButton} from "./LoadingButton";
 export {default as Navbar} from "./Navbar";
 export {default as NavSidebar} from "./NavSidebar";
+export {default as ProjectCard} from "./ProjectCard";
 export {default as ScrollContainer} from "./ScrollContainer";
