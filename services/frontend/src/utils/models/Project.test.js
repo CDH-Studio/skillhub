4001c4d42d17f317a2f2f38b00b73bf7af2785fa
Liannus <josh-gorman@hotmail.com>
2019-06-17 13:39:28 -0400

    CDHSH-80 Fixed test not needing toMatchObject anymore

--- services/frontend/src/utils/models/Project.test.js
+++ services/frontend/src/utils/models/Project.test.js
@@ -117,15 +117,6 @@ describe("mergeWithSkills", () => {
     const skillB = new Skill({id: "b", name: "test2"});
     const skillC = new Skill({id: "c", name: "test3"});
 
-    /* We delete the createdAt and updatedAt then use toMatchObject instead of toEqual
-    to test versus a subset of the properties (ex. name and id) */
-    delete skillA.createdAt;
-    delete skillB.createdAt;
-    delete skillC.createdAt;
-    delete skillA.updatedAt;
-    delete skillB.updatedAt;
-    delete skillC.updatedAt;
-
     const project1WithSkills = {...project1, skills: [skillA, skillB]};
     const project2WithSkills = {...project2, skills: [skillB, skillC]};
 
@@ -135,10 +126,10 @@ describe("mergeWithSkills", () => {
     const projectsWithSkills = [project1WithSkills, project2WithSkills];
 
     it("can merge a set of projects with a set of skills (while ignoring unknown skills)", () => {
-        expect(Project.mergeWithSkills(projects, skills)).toMatchObject(projectsWithSkills);
+        expect(Project.mergeWithSkills(projects, skills)).toEqual(projectsWithSkills);
     });
 
     it("returns an empty array when given empty inputs", () => {
-        expect(Project.mergeWithSkills()).toMatchObject([]);
+        expect(Project.mergeWithSkills()).toEqual([]);
     });
 });
