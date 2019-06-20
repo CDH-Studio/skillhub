6c6080037915d29466c4e3914888a3938656baf5
Devin Sit <devin@devinsit.com>
2019-06-20 11:02:22 -0400

    CDHSH-83 Just some cleanup of the scraper boilerplate.

--- services/scraper/src/middleware/index.js
+++ services/scraper/src/middleware/index.js
@@ -1,9 +1,7 @@
 const asyncMiddleware = require("./asyncMiddleware");
-const combineMiddleware = require("./combineMiddleware");
 const errorHandler = require("./errorHandler");
 
 module.exports = {
     asyncMiddleware,
-    combineMiddleware,
     errorHandler
 };
