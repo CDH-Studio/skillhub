8718e4fe69f6ea26e3ae25b30b027e2fce5f754a
Devin Sit <devin@devinsit.com>
2019-07-16 09:53:34 -0400

    CDHSH-96 Modified the JiraUser to reformat the name to 'First Last'.

--- services/scraper/src/utils/models/JiraUser.js
+++ services/scraper/src/utils/models/JiraUser.js
@@ -7,10 +7,19 @@ class JiraUser {
 
     toSkillhubUser() {
         return {
-            name: this.displayName,
+            name: reformatLastNameFirst(this.displayName),
             contactEmail: this.emailAddress
         };
     }
 }
+
+const reformatLastNameFirst = (name) => {
+    if (name.includes(",")) {
+        const splitName = name.split(",").map((n) => n.trim());
+        return `${splitName[1]} ${splitName[0]}`;
+    }
+
+    return name;
+};
 
 module.exports = JiraUser;
