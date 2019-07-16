b29e2ca3053ff199f0258e1a73758fdcc1a53b19
bmalghan <bmalg082@uottawa.ca>
2019-07-16 11:03:31 -0400

    CDHSH-100 pr changes and moved the sort to projectdetails.js

--- services/frontend/src/scenes/ProjectDetails/ProjectDetails.js
+++ services/frontend/src/scenes/ProjectDetails/ProjectDetails.js
@@ -3,7 +3,9 @@ import ProjectDetailsLayout from "./ProjectDetailsLayout";
 import connect from "./connect";
 
 const ProjectDetails = ({project, contributors, isLoading}) => (
-    <ProjectDetailsLayout project={project} contributors={contributors} isLoading={isLoading} />
+    <ProjectDetailsLayout project={project} contributors={sortContributors(contributors)} isLoading={isLoading} />
 );
+
+const sortContributors = (contributors) => contributors.sort((a, b) => a.name.localeCompare(b.name));
 
 export default connect(ProjectDetails);
