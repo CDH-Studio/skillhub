58c58634cba90d3237284c60c4ee8e00fa553682
Devin Sit <devin@devinsit.com>
2019-08-01 11:39:53 -0400

    CDHSH-111 Added the 'skills_controller' to the 'v1_controller'.

--- services/predictions/src/controllers/api/v1/v1_controller.py
+++ services/predictions/src/controllers/api/v1/v1_controller.py
@@ -1,5 +1,8 @@
 from utils.NestableBlueprint import NestableBlueprint
 from .contributors.contributors_controller import contributors_controller
+from .skills.skills_controller import skills_controller
 
 v1_controller = NestableBlueprint("v1", __name__, url_prefix="/v1")
+
 v1_controller.register_blueprint(contributors_controller)
+v1_controller.register_blueprint(skills_controller)
